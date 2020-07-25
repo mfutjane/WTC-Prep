4826dc4b303da2fb70eb5696a59fba3829d0d9d0
Mpho Futjane <56511252+the-imphoster@users.noreply.github.com>
2020-07-25 15:39:03 +0200

    Created EX10 and ft_swap.c
    Function swaps the values of the two given pointers. The temp variable holds the value for a, the value pointed to by a gets replaced with the value for b, then the value for b gets replaced with the original value for a (stored in temp).

---
+++ EX10/ft_swap.c
@@ -0,0 +1,8 @@
+void    ft_swap(int *a, int *b)
+{
+    int temp;
+
+    temp = *a //set temp to int value of a
+    *a = *b; //set value of a to value of b
+    *b = temp; //set value of b to temp
+}
