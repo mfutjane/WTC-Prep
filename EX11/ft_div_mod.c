4b90a61074df9f96d84e4ee74494d9ca0a1ad5e4
Mpho Futjane <56511252+the-imphoster@users.noreply.github.com>
2020-07-25 15:44:12 +0200

    Created EX11 and ft_div_mod.c
    Function calculates division of given ints (first by second) and the remainder of the division, storing them in div and mod respectively.

---
+++ EX11/ft_div_mod.c
@@ -0,0 +1,5 @@
+void    ft_div_mod(int a, int b, int *div, int *mod)
+{
+    *div = a/b; //divide a by b, store result as value for div
+    *mod = a%b; //divide a by b, store remainder as value for mod
+}
