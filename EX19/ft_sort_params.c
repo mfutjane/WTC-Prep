01c20f2d6deb7bdeac4c2863f50aa6ae97b1fe38
Mpho Futjane <56511252+the-imphoster@users.noreply.github.com>
2020-07-26 14:06:24 +0200

    Updated ft_sort_params.c
    Waiting for test

--- EX19/ft_sort_params.c
+++ EX19/ft_sort_params.c
@@ -25,6 +25,29 @@ int    ft_strcmp(char *str1, char *str2)
 
 void    selection_sort(int str_count, char **strings)
 {
+    char *temp;
+    int temp_min;
+    int min_candidate;
+    int unsorted_index;
+
+    temp_min = 0;
+    min_candidate = 1;
+    unsorted_index = 0;
+    while(unsorted_index < str_count - 1)
+    {
+        while(min_candidate < str_count)
+        {
+            if(ft_strcmp(strings[temp_min], strings[min_candidate]) > 0)
+                temp_min = min_candidate;
+        }
+        temp = argv[unsorted_index];
+        argv[unsorted_index] = argv[temp_min];
+        argv[temp_min] = temp;
+
+        unsorted_index++;
+        temp_min = unsorted_min;
+        min_candidate = unsorted_min + 1;
+    }
 }
 
 int    main(int argc, char **argv)
