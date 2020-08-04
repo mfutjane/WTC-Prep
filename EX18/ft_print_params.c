1d1670d65b27ff5b0a8a0a08ec97ec67c76c6338
Mpho Futjane <mfutjane@c5r2s10.wethinkcode.co.za>
2020-08-04 15:51:52 +0200

    Added main function, putstr and putchar code to print_params, tested.

--- EX18/ft_print_params.c
+++ EX18/ft_print_params.c
@@ -1,13 +1,27 @@
-int    main(int argc, char *argv)
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while(str[i] != '\0')
+		ft_putchar(str[i++]);
+	ft_putchar('\n');
+}
+
+int    main(int argc, char **argv)
 {
     int i;
 
     i = 1;
     if(argc > 1 && i < argc)
-    {
-        ft_putstr(argv[i]); //code for function in EX15
-        ft_putchar('\n'); //code for function in EX06
-        i++;
-    }
+		while (i < argc)
+        	ft_putstr(argv[i++]);
     return 0;
 }
