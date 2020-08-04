f41abc4f5b41b482733e05917d8148d5ca71fdda
Mpho Futjane <mfutjane@c5r2s10.wethinkcode.co.za>
2020-08-04 15:46:07 +0200

    Tested and added main function to strcmp.

--- EX17/ft_strcmp.c
+++ EX17/ft_strcmp.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 int    ft_strcmp(char *s1, char *s2)
 {
     int i;
@@ -12,4 +14,23 @@ int    ft_strcmp(char *s1, char *s2)
         i++;
     }
     return 0;
+}
+
+int	main(int argc, char **argv)
+{
+	int res;
+
+	if(argc == 3)
+	{
+		res = ft_strcmp(argv[1], argv[2]);
+		if(res == 1)
+			printf("%s is bigger than %s\n", argv[1], argv[2]);
+		else if(res == -1)
+			printf("%s is less than %s\n", argv[1], argv[2]);
+		else
+			printf("%s is the same as %s\n", argv[1], argv[2]);
+	}
+	else
+		printf("Wrong number of arguments. Please only write 2 strings to compare\n");
+	return 0;
 }
