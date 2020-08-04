d2f3b2ee58226c1675ee3bbe099201b306cfffdf
Mpho Futjane <mfutjane@c5r2s10.wethinkcode.co.za>
2020-08-04 16:21:14 +0200

    Added test and fixed bug in ft_range

--- EX21/ft_range.c
+++ EX21/ft_range.c
@@ -1,4 +1,17 @@
-//include stdlib.h to use malloc and null
+#include <stdlib.h>
+#include <stdio.h>
+
+void	print_list(int *list, int len)
+{
+	int i;
+
+	i = 0;
+	while(i < len)
+	{
+		printf("Digit num %d is the number: %d\n", i+1, list[i]);
+		i++;
+	}
+}
 
 int    *ft_range(int min, int max)
 {
@@ -6,14 +19,25 @@ int    *ft_range(int min, int max)
     int difference;
     int *ret;
 
+	i = 0;
     difference = max - min;
+	printf("diff is %d\n", difference);
     if(difference <= 0)
         return NULL;
-    ret = (int*)malloc(sizeof(int)*difference);
+    ret = (int*)malloc(sizeof(int)*difference + 1);
     while(i < difference)
     {
         ret[i] = min + i;
-        i++;
+        i++; 
     }
     return ret;
+}
+
+int	main(void)
+{
+	int	*list;
+
+	list = ft_range(-4, 8);
+	print_list(list, 12);
+	return 0;
 }
