209746378f20fb46c50fc6e04a6f7594fd2d8356
Mpho Futjane <mfutjane@c5r2s10.wethinkcode.co.za>
2020-08-04 12:24:07 +0200

    Tested ex11/ft_div_mod, added main function

--- EX11/ft_div_mod.c
+++ EX11/ft_div_mod.c
@@ -1,5 +1,17 @@
+#include <stdio.h>
+
 void    ft_div_mod(int a, int b, int *div, int *mod)
 {
     *div = a/b; //divide a by b, store result as value for div
     *mod = a%b; //divide a by b, store remainder as value for mod
+}
+
+int	main(void)
+{
+	int	div;
+	int mod;
+
+	ft_div_mod(30, 7, &div, &mod);
+	printf("div %d mod %d", div, mod);
+	return 0;
 }
