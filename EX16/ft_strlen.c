9238dba96af711a7fb7017e6eb465f30a470abe5
Mpho Futjane <mfutjane@c5r2s10.wethinkcode.co.za>
2020-08-04 15:32:49 +0200

    Modified strlen to have tests, and print out the number of characters in each arg

--- EX16/ft_strlen.c
+++ EX16/ft_strlen.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 int    ft_strlen(char *str)
 {
     int counter;
@@ -8,4 +10,19 @@ int    ft_strlen(char *str)
         counter++;
     }
     return counter;
+}
+
+//prints out the number of characters in a string except the program name
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	while(i < argc && argc > 1)
+	{
+			
+			printf("String %d has %d characters\n", i, ft_strlen(argv[i]));
+			i++;
+	}
+	return 0;
 }
