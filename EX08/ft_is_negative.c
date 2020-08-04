cde7dd6edb5088fc0a53ea8f5acfdbbc7c23ae33
Mpho Futjane <mfutjane@c5r2s10.wethinkcode.co.za>
2020-08-04 12:07:26 +0200

    Added tests to ft_is_negative, function also now prints new line after output

--- EX08/ft_is_negative.c
+++ EX08/ft_is_negative.c
@@ -1,7 +1,22 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
 void    ft_is_negative(int n)
 {
     if(n < 0)
         ft_putchar('N');
-    else
-        ft_putchar('P');
+	else
+		ft_putchar('P');
+	ft_putchar('\n');
+}
+
+int	main(void)
+{
+	ft_is_negative(9);
+	ft_is_negative(-3);
+	return 0;
 }
