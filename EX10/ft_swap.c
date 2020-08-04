af556c80142882e3ab64117942e278f1ff14ef9a
Mpho Futjane <mfutjane@c5r2s10.wethinkcode.co.za>
2020-08-04 12:18:52 +0200

    Tested ex10/ft_swap.c, added main function

--- EX10/ft_swap.c
+++ EX10/ft_swap.c
@@ -1,8 +1,22 @@
+#include <stdio.h>
+
 void    ft_swap(int *a, int *b)
 {
     int temp;
 
-    temp = *a //set temp to int value of a
+    temp = *a; //set temp to int value of a
     *a = *b; //set value of a to value of b
     *b = temp; //set value of b to temp
+}
+
+int	main(void)
+{
+	int	uno;
+	int	dos;
+
+	uno = 0;
+	dos = 4353234;
+	ft_swap(&uno, &dos);
+	printf("uno %d dos %d dos\n", uno, dos);
+	return 0;
 }
