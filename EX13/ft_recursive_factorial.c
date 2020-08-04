16658331117d71708d36def840e2af059d8ee41c
Mpho Futjane <mfutjane@c5r2s10.wethinkcode.co.za>
2020-08-04 14:17:51 +0200

    Fixed error for 0! in recursive function, now returns 1. Added main function and tested.

--- EX13/ft_recursive_factorial.c
+++ EX13/ft_recursive_factorial.c
@@ -1,8 +1,18 @@
+#include <stdio.h>
+
 int    ft_recursive_factorial(int nb)
 {
-    if(nb == 1)
+    if(nb == 0)
         return 1;
     if(nb < 0)
         return 0;
     return nb * ft_recursive_factorial(nb-1);
+}
+
+int	main(void)
+{
+	printf("%d\n", ft_recursive_factorial(0));
+	printf("%d\n", ft_recursive_factorial(5));
+	printf("%d\n", ft_recursive_factorial(-123));
+	return 0;	
 }
