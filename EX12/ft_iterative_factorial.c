a83cebb928080dc1fb13185a5403a082e24ced88
Mpho Futjane <mfutjane@c5r2s10.wethinkcode.co.za>
2020-08-04 14:12:46 +0200

    Tested iterative factorial, added main and 3 small tests

--- EX12/ft_iterative_factorial.c
+++ EX12/ft_iterative_factorial.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 int    ft_iterative_factorial(int nb)
 {
     int result;
@@ -11,4 +13,12 @@ int    ft_iterative_factorial(int nb)
         nb--;
     }
     return result;
+}
+
+int	main(void)
+{
+	printf("%d\n", ft_iterative_factorial(0));
+	printf("%d\n", ft_iterative_factorial(5));
+	printf("%d\n", ft_iterative_factorial(-23));
+	return 0;
 }
