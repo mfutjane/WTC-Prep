631eb0dab18334d4df539581dcb499b3d5dcef28
Mpho Futjane <mfutjane@c5r2s10.wethinkcode.co.za>
2020-08-04 16:29:49 +0200

    Added test to ft_foreach

--- EX25/ft_foreach.c
+++ EX25/ft_foreach.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 void    ft_foreach(int *tab, int length, void(*f)(int))
 {
     int i;
@@ -8,4 +10,20 @@ void    ft_foreach(int *tab, int length, void(*f)(int))
         (*f)(tab[i]);
         i++;
     }
+}
+
+void multiply_by_itself(int n)
+{
+	int ret;
+
+	ret = n*n;
+	printf("%d is the result of %d*%d\n", ret, n, n);
+}
+
+int	main(void)
+{
+	int list[5] = {1,2,3,4,10};
+
+	ft_foreach(list, 5, &multiply_by_itself);
+	return 0;
 }
