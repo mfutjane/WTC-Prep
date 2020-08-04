e6a376686789bd6c4a8ed95fedc25bcf38f6dba0
Mpho Futjane <mfutjane@c5r2s10.wethinkcode.co.za>
2020-08-04 12:02:14 +0200

    Updated ft_print_num, now
    has main and putchar functions

--- EX07/ft_print_numbers.c
+++ EX07/ft_print_numbers.c
@@ -1,3 +1,10 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
 void    ft_print_numbers(void)
 {
     char num_to_print;
@@ -5,4 +12,10 @@ void    ft_print_numbers(void)
     num_to_print = '0';
     while(num_to_print <= '9')
         ft_putchar(num_to_print++);
+}
+
+int	main(void)
+{
+	ft_print_numbers();
+	return 0;
 }
